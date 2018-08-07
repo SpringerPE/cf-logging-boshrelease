47d9ff03cd07d829e5714bbc61d00b6321fb1244
Jose Riguera <jriguera@gmail.com>
2018-08-07 16:25:18 +0200

    Add logstash_exporter operations job

--- manifest/interpolate-manifest.sh
+++ manifest/interpolate-manifest.sh
@@ -9,6 +9,7 @@ $BOSH_CLI int logstash.yml \
     -o operations/pipelines/cf-platform-es.yml \
     -o operations/add-es-cloud-id.yml \
     -o operations/add-es-xpack.yml \
+    -o operations/add-logstash-exporter.yml \
     -o operations/add-release-version.yml  --vars-file vars-release-version.yml \
     -o operations/add-iaas-parameters.yml  --vars-file vars-iaas-parameters.yml \
     # --vars-store secrets-example.yml
