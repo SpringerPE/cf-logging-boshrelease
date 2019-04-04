1703a08ede93b28a939000b9c8a4ae0a8f38a615
SuchiSmita-N <suchismita.nayak@springer.com>
2019-04-04 17:01:38 +0200

    Configured DLQ in test-logstash

--- manifest/operations/pipelines/generate_pipeline_operations.sh
+++ manifest/operations/pipelines/generate_pipeline_operations.sh
@@ -88,6 +88,7 @@ add_oper_snippets() {
         queue.type: persisted
         queue.max_bytes: 90gb
         path.queue: /var/vcap/store/logstash
+        dead_letter_queue.enable: true
       config:
 OperSNIPPETS
 
@@ -115,7 +116,7 @@ list_snippets() {
     while IFS=  read -r -d $'\0' line
     do
         snippets+=($(basename "${line}"))
-    done < <(find ${folder} -xtype f -name "*.conf" -print0 | sort -z)
+    done < <(find ${folder} -type f -name "*.conf" -print0 | sort -z)
     for i in ${snippets[@]}
     do
         echo "${i}"
